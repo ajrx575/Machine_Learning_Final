a084b368e6dc916740ea48cb8be3a4164a30846d
Adia <adia@Adias-MacBook-Pro.local>
2025-11-08 17:23:22 -0600

    Initial commit

---
+++ ML/Final/FP_file.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import numpy as np
+
+input_file_path = 'dataJAPPL-2021-00246.csv'
+output_file_path = 'cleaned_coffee.csv'
+
+with open(input_file_path, 'r') as infile, open(output_file_path, 'w') as outfile:
+    for line in infile:
+        # Replace all occurrences of '""' with an empty string
+        cleaned_line = line.replace('"', '')
+        outfile.write(cleaned_line)
+
+coffeedf = pd.read_csv('cleaned_coffee.csv', delimiter=';')
